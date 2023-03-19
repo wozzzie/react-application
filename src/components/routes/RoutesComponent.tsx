2001e710194f0d0fe9b4d1e217b30974e1aec25c
Maria Gornashevich <lightbrown9874@gmail.com>
2023-03-20 00:16:13 +0300

    feat: add error page

--- src/components/routes/RoutesComponent.tsx
+++ src/components/routes/RoutesComponent.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import About from "../../screens/about/About";
-
+import Error from "../../screens/error/Error";
 class RoutesComponent extends Component {
   render(): JSX.Element {
     return (
@@ -17,6 +17,13 @@ class RoutesComponent extends Component {
             "
               message="We offer a wide selection of indoor and outdoor plants. Plants not only enhance your surroundings but also improve your well-being. We are committed to providing high-quality plants. Let us help you find the perfect plant for your space and provide you with the knowledge and resources to help it thrive. Thank you for choosing our Plants Store!"
             />
+          }
+        />
+        <Route
+          data-testid="error-page"
+          path="*"
+          element={
+            <Error errorMessage="Oops! Something went wrong. Please try again later." />
           }
         />
       </Routes>
