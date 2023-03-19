f914fc9ba2514ffb5ddfb57d07409f67134ff2aa
Maria Gornashevich <lightbrown9874@gmail.com>
2023-03-20 00:44:42 +0300

    feat: add data for cards

---
+++ src/data/data.ts
@@ -0,0 +1,178 @@
+export const DATA = [
+  {
+    id: "8Rp2S7QXjkTy",
+    title: "Peperomia Ginny",
+    author: "Leona Crisp",
+    description:
+      "Peperomia ginny is a colorful species in the Piperaceae, or pepper, family. Native to Venezuela and West Indies, they are easy to care for houseplants that add a welcome burst of color to any room.",
+    location: "Venezuela",
+    likes: "2304",
+    requirements:
+      "Peperomia ginny light preference is for indirect bright light, but it can withstand medium light levels. However, a Peperomia with pink edges will have more vibrant colors if it receives bright light.",
+  },
+  {
+    id: "4Mn9B6ZLecFh",
+    title: "Bird’s Nest Fern",
+    author: "David Marse",
+    description:
+      "Bird's nest ferns are naturally epiphytic, meaning they grow on the surface of other plants. In their rainforest homes, they can be found growing high in the crooks of trees.",
+    location: "Australia",
+    likes: "4454",
+    requirements:
+      "The key to a healthy bird's nest fern is providing it with ample warmth, humidity, and moisture.",
+  },
+  {
+    id: "1Kv0A3YDwgUi",
+    title: "Snake Plant",
+    author: "plant_obsessed",
+    description:
+      "Snake plant, also known as Sansevieria, is a hardy and low maintenance plant with long, upright leaves that grow straight up like spears. Originating from West Africa, it's a great choice for beginners or for those who have limited space and time for plant care.",
+    location: "West Africa",
+    likes: "1921",
+    requirements:
+      "Snake plants can tolerate a wide range of light conditions, from low to bright indirect light. Water sparingly and only when the soil is completely dry, as they are prone to root rot.",
+  },
+  {
+    id: "7Fp5R2JNxOqV",
+    title: "ZZ Plant",
+    author: "green_thumb",
+    description:
+      "ZZ plant, also known as Zamioculcas zamiifolia, is a popular houseplant with glossy, dark green leaves that resemble feathers. Native to Africa, it's a hardy and low maintenance plant that can tolerate neglect and low light conditions.",
+    location: "Africa",
+    likes: "2796",
+    requirements:
+      "ZZ plants can tolerate a wide range of light conditions, from low to bright indirect light. Water sparingly and only when the soil is completely dry, as they are prone to root rot.",
+  },
+  {
+    id: "3Wl8U9EIbTzG",
+    title: "Pothos",
+    author: "plant_addict",
+    description:
+      "Pothos, also known as Devil's Ivy, is a popular trailing plant with heart-shaped leaves that come in a variety of colors and patterns. Native to the Solomon Islands, it's a great choice for beginners or for those who want a plant that can be easily propagated.",
+    location: "Solomon Islands",
+    likes: "4125",
+    requirements:
+      "Pothos can tolerate a wide range of light conditions, from low to bright indirect light. Water when the top inch of soil is dry and allow excess water to drain away.",
+  },
+  {
+    id: "6Yh2N1KdVmZx",
+    title: "Fiddle Leaf Fig",
+    author: "plant_enthusiast",
+    description:
+      "Fiddle Leaf Fig, also known as Ficus lyrata, is a popular houseplant with large, fiddle-shaped leaves that add a tropical touch to any room. Native to western Africa, it requires a bit more care and attention than some other houseplants.",
+    location: "Western Africa",
+    likes: "3467",
+    requirements:
+      "Fiddle Leaf Figs require bright, indirect light and consistent moisture. Water when the top inch of soil is dry and rotate the plant periodically to ensure even growth.",
+  },
+  {
+    id: "9Gj7B0CfLaRt",
+    title: "Bird of Paradise",
+    author: "plant_lover",
+    description:
+      "Bird of Paradise, also known as Strelitzia reginae, is a tropical plant with vibrant orange and blue flowers that resemble a bird in flight. Native to South Africa, it's a showstopper of a plant that requires a bit of care to thrive indoors.",
+    location: "South Africa",
+    likes: "2118",
+    requirements:
+      "Bird of Paradise requires bright, indirect light and consistently moist soil. They also benefit from regular misting and should be fertilized monthly during the growing season.",
+  },
+  {
+    id: "2Tc5E4QpXuMo",
+    title: "Rubber Plant",
+    author: "green_thumb",
+    description:
+      "Rubber Plant, also known as Ficus elastica, is a popular houseplant with glossy, dark green leaves that can grow up to a foot long. Native to Southeast Asia, it's a hardy and low maintenance plant that can thrive in a variety of conditions.",
+    location: "Southeast Asia",
+    likes: "2975",
+    requirements:
+      "Rubber plants prefer bright, indirect light but can also tolerate low light conditions. Water when the top inch of soil is dry and mist the leaves occasionally to increase humidity.",
+  },
+  {
+    id: "5Zm1W8VrPyNk",
+    title: "Aloe Vera",
+    author: "plant_addict",
+    description:
+      "Aloe Vera is a popular succulent plant with thick, spiky leaves that are known for their healing properties. Native to North Africa, it's an easy to care for plant that thrives in bright, indirect light.",
+    location: "North Africa",
+    likes: "3752",
+    requirements:
+      "Aloe Vera prefers bright, indirect light and well-draining soil. Water sparingly and only when the soil is completely dry, as they are prone to root rot.",
+  },
+  {
+    id: "0Dv6L3UaGtIx",
+    title: "Spider Plant",
+    author: "plant_obsessed",
+    description:
+      "Spider Plant, also known as Chlorophytum comosum, is a popular houseplant with long, slender leaves that grow in a cascading pattern. Native to tropical and southern Africa, it's a great choice for beginners or for those who want a plant that's easy to propagate.",
+    location: "Tropical and Southern Africa",
+    likes: "2386",
+    requirements:
+      "Spider plants prefer bright, indirect light but can also tolerate low light conditions. Water when the top inch of soil is dry and allow excess water to drain away.",
+  },
+  {
+    id: "7Qs2Z9EjKbMh",
+    title: "Philodendron Brasil",
+    author: "plant_enthusiast",
+    description:
+      "Philodendron Brasil is a popular houseplant with heart-shaped leaves that are variegated with shades of green and yellow. Native to South America, it's an easy to care for plant that can thrive in a variety of conditions.",
+    location: "South America",
+    likes: "2841",
+    requirements:
+      "Philodendron Brasil prefers bright, indirect light but can also tolerate low light conditions. Water when the top inch of soil is dry and allow excess water to drain away.",
+  },
+  {
+    id: "3Lc5G6FtVnKu",
+    title: "Pilea Peperomioides",
+    author: "plant_addict",
+    description:
+      "Pilea Peperomioides, also known as the Chinese Money Plant, is a popular houseplant with round, coin-shaped leaves that grow on long stems. Native to Southern China, it's a relatively new plant in the houseplant world but has gained popularity for its unique appearance.",
+    location: "Southern China",
+    likes: "2278",
+    requirements:
+      "Pilea Peperomioides prefers bright, indirect light and well-draining soil. Water when the top inch of soil is dry and mist the leaves occasionally to increase humidity.",
+  },
+  {
+    id: "1Tm9X0NpRyOe",
+    title: "Monstera Deliciosa",
+    author: "plant_enthusiast",
+    description:
+      "Monstera Deliciosa, also known as the Swiss Cheese Plant, is a popular houseplant with large, perforated leaves that can grow up to 3 feet long. Native to the tropical forests of Central and South America, it's a stunning plant that requires a bit of care to thrive indoors.",
+    location: "Central and South America",
+    likes: "3985",
+    requirements:
+      "Monstera Deliciosa prefers bright, indirect light and well-draining soil. Water when the top inch of soil is dry and mist the leaves occasionally to increase humidity. Provide support for the plant as it grows, as it can become quite large.",
+  },
+  {
+    id: "6Bv4K7YjHgUi",
+    title: "Fiddle Leaf Fig",
+    author: "plant_obsessed",
+    description:
+      "Fiddle Leaf Fig, also known as Ficus lyrata, is a popular houseplant with large, violin-shaped leaves that can grow up to 18 inches long. Native to Western Africa, it's a stunning plant that requires a bit of care to thrive indoors.",
+    location: "Western Africa",
+    likes: "4298",
+    requirements:
+      "Fiddle Leaf Figs prefer bright, indirect light and well-draining soil. Water when the top inch of soil is dry and mist the leaves occasionally to increase humidity. Rotate the plant occasionally to ensure even growth.",
+  },
+  {
+    id: "9Dx3F2PqEaVl",
+    title: "Boston Fern",
+    author: "plant_crazy",
+    description:
+      "Boston Fern, also known as Nephrolepis exaltata, is a classic houseplant with long, delicate fronds that arch gracefully. Native to tropical regions worldwide, it's a great choice for those who want to add a touch of greenery to their home.",
+    location: "Tropical regions worldwide",
+    likes: "1765",
+    requirements:
+      "Boston Ferns prefer bright, indirect light and high humidity. Water regularly and mist the leaves often to increase humidity. They also benefit from occasional fertilizer and repotting.",
+  },
+  {
+    id: "2Sf1N6GkYbWu",
+    title: "String of Pearls",
+    author: "plant_addict",
+    description:
+      "String of Pearls, also known as Senecio rowleyanus, is a unique and eye-catching trailing plant with small, round leaves that resemble pearls on a string. Native to South Africa, it's a great choice for hanging baskets or as a trailing accent plant.",
+    location: "South Africa",
+    likes: "2546",
+    requirements:
+      "String of Pearls prefer bright, indirect light and well-draining soil. Water sparingly and only when the soil is completely dry, as they are prone to root rot. Provide support for the plant as it grows, as it can become quite long and heavy.",
+  },
+];
