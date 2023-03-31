fc72bbc66997517b56c1fae3ec5df78df005f07c
Maria Gornashevich <lightbrown9874@gmail.com>
2023-03-31 18:11:20 +0300

    refactor: rewrite SearchBar component into functional

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,45 +1,39 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import './SearchBar.css';
 
-class SearchBar extends React.Component {
-  state = {
-    search: '',
-  };
+const SearchBar = () => {
+  const [search, setSearch] = useState('');
 
-  handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ search: event.target.value });
-  };
-
-  componentDidMount = () => {
+  useEffect(() => {
     const searchStorage = localStorage.getItem('searchForm');
-
     if (searchStorage) {
-      this.setState({ search: searchStorage });
+      setSearch(searchStorage);
     }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('searchForm', search);
+  }, [search]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
   };
 
-  componentWillUnmount = () => {
-    localStorage.setItem('searchForm', this.state.search);
-  };
-
-  render() {
-    return (
-      <div className="search-form__wrapper">
-        <form className="search-form">
-          <input
-            data-testid="search-input"
-            className="search-form__input"
-            type="search"
-            value={this.state.search}
-            onChange={this.handleSearchChange}
-            placeholder="Search for a plant..."
-          />
-          <img src="/img/search.png" alt="loop" className="search-form__loop" />
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-form__wrapper">
+      <form className="search-form">
+        <input
+          data-testid="search-input"
+          className="search-form__input"
+          type="search"
+          value={search}
+          onChange={handleSearchChange}
+          placeholder="Search for a plant..."
+        />
+        <img src="/img/search.png" alt="loop" className="search-form__loop" />
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
