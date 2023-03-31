ae44ae3e5f67fb55762d59827c12f08706017f3a
Maria Gornashevich <lightbrown9874@gmail.com>
2023-03-31 19:49:07 +0300

    refactor: rewrite Header  component into functional

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, UNSAFE_LocationContext } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import './Header.css';
 
@@ -9,60 +9,62 @@ type HeaderProps = {
   card: string;
 };
 
-class Header extends React.Component<HeaderProps> {
-  render(): JSX.Element {
-    const { home, about, card } = this.props;
-    return (
-      <div className="container">
-        <header className="header">
-          <div>
-            <UNSAFE_LocationContext.Consumer>
-              {(value) => (
-                <p className="header__title">
-                  {value.location.pathname === '/'
-                    ? 'Home'
-                    : value.location.pathname === '/about'
-                    ? 'About'
-                    : value.location.pathname === '/card'
-                    ? 'Your card'
-                    : '404'}
-                </p>
-              )}
-            </UNSAFE_LocationContext.Consumer>
-          </div>
-          <div className="header__links">
-            <NavLink
-              to="/"
-              data-testid="main-link"
-              className={({ isActive }): string =>
-                isActive ? 'header__link_active' : 'header__link'
-              }
-            >
-              {home}
-            </NavLink>
-            <NavLink
-              to="/about"
-              data-testid="about-link"
-              className={({ isActive }): string =>
-                isActive ? 'header__link_active' : 'header__link'
-              }
-            >
-              {about}
-            </NavLink>
-            <NavLink
-              to="/card"
-              data-testid="card-link"
-              className={({ isActive }): string =>
-                isActive ? 'header__link_active' : 'header__link'
-              }
-            >
-              {card}
-            </NavLink>
-          </div>
-        </header>
-      </div>
-    );
+const Header: React.FC<HeaderProps> = ({ home, about, card }) => {
+  const location = useLocation();
+
+  let title: string;
+  switch (location.pathname) {
+    case '/':
+      title = 'Home';
+      break;
+    case '/about':
+      title = 'About';
+      break;
+    case '/card':
+      title = 'Your card';
+      break;
+    default:
+      title = '404';
   }
-}
+
+  return (
+    <div className="container">
+      <header className="header">
+        <div>
+          <p className="header__title">{title}</p>
+        </div>
+        <div className="header__links">
+          <NavLink
+            to="/"
+            data-testid="main-link"
+            className={({ isActive }): string =>
+              isActive ? 'header__link_active' : 'header__link'
+            }
+          >
+            {home}
+          </NavLink>
+          <NavLink
+            to="/about"
+            data-testid="about-link"
+            className={({ isActive }): string =>
+              isActive ? 'header__link_active' : 'header__link'
+            }
+          >
+            {about}
+          </NavLink>
+          <NavLink
+            to="/card"
+            data-testid="card-link"
+            className={({ isActive }): string =>
+              isActive ? 'header__link_active' : 'header__link'
+            }
+          >
+            {card}
+          </NavLink>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
