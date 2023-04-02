22cbbf648d5da6b0a79db3b4b648e53c15d45e95
Maria Gornashevich <lightbrown9874@gmail.com>
2023-04-02 23:55:03 +0300

    refactor: change textarea component into functional

--- src/components/form/textarea/index.tsx
+++ src/components/form/textarea/index.tsx
@@ -1,4 +1,6 @@
-import React, { RefObject } from 'react';
+import React from 'react';
+import { UseFormRegister } from 'react-hook-form';
+import { FormValues } from '../../../types/form';
 
 import './style.css';
 import '../index.css';
@@ -7,31 +9,36 @@ interface TextAreaProps {
   label: string;
   defaultValue: string;
   error?: string;
-  textAreaRef?: RefObject<HTMLTextAreaElement>;
+  register: UseFormRegister<FormValues>;
+  name: keyof FormValues;
 }
 
-class TextArea extends React.Component<TextAreaProps> {
-  render() {
-    const { label, defaultValue, error, textAreaRef } = this.props;
-    return (
-      <div className="block">
-        <label htmlFor="textarea" className="label">
-          {label}
-        </label>
-        <textarea
-          id="textarea"
-          className="textarea"
-          defaultValue={defaultValue}
-          ref={textAreaRef}
-        />
-        {error && (
-          <div style={{ color: 'red' }} className="form__error" data-testid="form-error">
-            {error}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const TextArea = ({ label, defaultValue, error, register, name }: TextAreaProps) => {
+  return (
+    <div className="block">
+      <label htmlFor={name} className="label">
+        {label}
+      </label>
+      <textarea
+        id={name}
+        className="textarea"
+        defaultValue={defaultValue}
+        {...register(name, {
+          required: 'Field is required',
+          maxLength: {
+            value: 200,
+            message: 'Max length is 200 characters',
+          },
+        })}
+      />
+
+      {error && (
+        <div style={{ color: 'red' }} className="form__error" data-testid="form-error">
+          {error}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default TextArea;
