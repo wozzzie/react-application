9e6c4135fce0700f81264a405bc226bcdb1de742
Maria Gornashevich <lightbrown9874@gmail.com>
2023-04-09 20:48:29 +0300

    fix: logic for displaying cards and add default text

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -3,6 +3,8 @@ import { CardType } from '../../types/form';
 import Card from '../../components/card/Card';
 import SearchBar from '../../components/searchBar/SearchBar';
 import './Home.css';
+import { data } from '../../data/data';
+import filteredData from '../../components/filteredData/filteredData';
 
 interface HomeProps {
   advice: string;
@@ -10,18 +12,13 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = ({ advice }) => {
   const [cards, setCards] = useState<CardType[]>([]);
-  const [search, setSearch] = useState<string>('');
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
-  };
+  const [search, setSearch] = useState('');
 
   const handleSearchSubmit = async (searchTerm: string) => {
     const response = await fetch(`https://mock-server-api-two.vercel.app/catalog?q=${searchTerm}`);
     const data = await response.json();
     setCards(data);
-    localStorage.setItem('searchForm', searchTerm);
-    setSearch('');
+    setSearch(searchTerm);
   };
 
   useEffect(() => {
@@ -29,7 +26,18 @@ const Home: React.FC<HomeProps> = ({ advice }) => {
     if (searchForm) {
       setSearch(searchForm);
     }
+    const defaultCards = searchForm ? filteredData(data, searchForm) : data;
+    setCards(defaultCards);
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('searchForm', search);
+  }, [search]);
+
+  useEffect(() => {
+    const filteredCards = filteredData(data, search);
+    setCards(filteredCards);
+  }, [search]);
 
   return (
     <div data-testid="main-page">
@@ -37,18 +45,22 @@ const Home: React.FC<HomeProps> = ({ advice }) => {
       <div className="container">
         <div className="home__wrapper">
           <div className="home__card">
-            {cards.map((card) => (
-              <Card
-                key={card.id}
-                image={card.image}
-                title={card.title}
-                author={card.author}
-                description={card.description}
-                location={card.location}
-                likes={card.likes}
-                requirements={card.requirements}
-              />
-            ))}
+            {cards.length === 0 ? (
+              <div className="home__no-results">No results found for &quot;{search}&quot;</div>
+            ) : (
+              cards.map((card) => (
+                <Card
+                  key={card.id}
+                  image={card.image}
+                  title={card.title}
+                  author={card.author}
+                  description={card.description}
+                  location={card.location}
+                  likes={card.likes}
+                  requirements={card.requirements}
+                />
+              ))
+            )}
           </div>
           <div className="home__text">
             <div className="home__advice">{advice}</div>
